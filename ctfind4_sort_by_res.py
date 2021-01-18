2cca775228c8426261851254dfb88bdd78ecaade
Israel Fernandez <76214130+PirateFernandez@users.noreply.github.com>
2021-01-18 13:07:26 +0100

    Update ctfind4_sort_by_res.py

--- ctfind4_sort_by_res.py
+++ ctfind4_sort_by_res.py
@@ -1,6 +1,6 @@
 import os
 import sys
-""" check if the user supplied path to log files, if not handle the exception"""
+""" check if the user supplied path to log files, if not handle the exception."""
 try:
 	filepath_1 = sys.argv[1]
 	
@@ -10,10 +10,11 @@ except IndexError:
 	print("pythonn3.x ctfind4_sort_by_res.py /path/path/relion_dir/CtfFind/jobxxx/yyy res_limit")
 	print("Please provide a directory path.")
 	exit()
+"""check if the user supplied a resolution limit, if not handle the exception."""
 try:
 	resolution_limit = float(sys.argv[2])
 except IndexError:
-	print("Wrong usage, please input path to ctfind4.log files.")
+	print("Wrong usage, please input desired resolution limit.")
 	print("Correct usage:")
 	print("pythonn3.x ctfind4_sort_by_res.py /path/path/relion_dir/CtfFind/jobxxx/yyy res_limit")
 	print("Please provide a resolution limit.")
@@ -24,17 +25,17 @@ except IndexError:
 file_list = os.listdir(filepath_1)
 file_list_2 = []
 file_list_3 = []
-
+"""loop to generate lists with only ctffind4.log files (list_2)"""
 for i in file_list:
 	if '_ctffind4.log' in i:
 		file_list_2.append(i)
-
+"""loop to generate list path+ctffind4.log (list_3)"""
 for i in file_list:
 	if '_ctffind4.log' in i:
 		i2 = f"{filepath_1}/{i}"
 		file_list_3.append(i2)
 """define a function to extract the resolution line from ctffind4.log's"""
-"""input is a list of path+ctffind4.log and returns is a list of lines 'Thon rings with good fit up to  : XXXX'"""
+"""input is a list of path+ctffind4.log and it returns a list of lines 'Thon rings with good fit up to  : XXXX'"""
 def extract_res_line(logfilelist):
 	res_list = []
 	for i in logfilelist:
@@ -44,15 +45,15 @@ def extract_res_line(logfilelist):
 				if 'Thon rings with good fit up to  :' in i2:
 					res_list.append(i2)
 	return res_list
-"""store the list of lines with resolution values in list res_list_2"""
+"""using function extract_res_line() store the list of lines with resolution values in list res_list_2"""
 res_list_2 = extract_res_line(file_list_3)
 """generate a list with only the res values followed by Angstroms"""
 res_list_3 = []
 for i in res_list_2:
 	i_split = i.split(":")
 	res_list_3.append(i_split[-1])
-"""create a dictionary from to lists"""
-"""map name of the ctffind4.log to the res value"""
+"""create a dictionary from two lists"""
+"""maps name of the ctffind4.log to the res value"""
 dic_img_res = dict(zip(file_list_2, res_list_3))
 """print in a nice format the key and values from dict dic_img_res"""
 #for key, value in dic_img_res.items():
@@ -64,7 +65,7 @@ for i in res_list_3:
 	res_list_4.append(float(i_split[-2]))
 """generate a second dictionary mapping log file names (str) to res (floats)"""
 dic_img_res_2 = dict(zip(file_list_2, res_list_4))
-"""print only those logs files with resolution values better than a especified number"""
+"""print only those logs files with resolution values better than the especified res number"""
 for key, value in dic_img_res_2.items():
 	if value < resolution_limit:
 		print((f"Image {key} reach resolution beyond {resolution_limit}.").strip())
