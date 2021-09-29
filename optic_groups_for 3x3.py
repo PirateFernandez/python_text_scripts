ce6048fa41c607ea7b59baa3f830cc8f77a37f63
Israel Fernandez <76214130+PirateFernandez@users.noreply.github.com>
2021-09-29 16:22:20 +0200

    Update optic_groups_for 3x3.py

--- optic_groups_for 3x3.py
+++ optic_groups_for 3x3.py
@@ -8,11 +8,16 @@ def number_split(filename):
 ###
 #pcs = subprocess.run(["mv", f"{i}"], f"{current[-2]}_g0.tif")
 dirpath = '/cryoem-data3/ISF/ApoFe_200kev_sept/tiff'
+####you have to place here the absolute path to the directory where the tif files are located and execute this script in the same directory###
 fileslist = os.listdir(dirpath)
+###little loop ove the contains of the filelist list to extract only tif files###
 tiflist = []
 for i in fileslist:
 	if '.tif' in i:
 		tiflist.append(i)
+###in this case I want to rename each file sequentially in batches of 9 as we took 3x3=9 images per stage shift in serial EM. I create 9 different list of numbers
+### each outstepped by 1 and then I will compare the serialEM number flag (which is added sequentially) agaist these 9 list. Finally. I will access the system via
+### subprocess method to run the linux cmd mv X Y
 list_0 = [ i for i in range(0,100, 9) ]
 list_1 = [ i+1 for i in range(0,100, 9) ]
 list_2 = [ i+2 for i in range(0,100, 9) ]
@@ -60,4 +65,3 @@ for i in tiflist:
 		current = i.split(".")
 		subprocess.run(["mv", f"{i}", f"{current[-2]}_g8.tif"])
 		#print(f"mv {i} {current[-2]}_g8.tif")
-print(Done!)
